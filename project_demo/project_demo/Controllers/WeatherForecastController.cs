58d3d7b74abc6ab61577840353071c01c1af2342
Quang Huynh <nhutquang111101@gmail.com>
2023-03-09 10:09:15 +0700

    Huynh Nhut Quang _ 1911061385

--- project_demo/project_demo/Controllers/WeatherForecastController.cs
+++ project_demo/project_demo/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace project_demo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracinggggggggggggggggggg", "Chilly", "Cool", "Mild", "Warmsssssssssss", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracinggggggggggggggggggg", "Chilly_ Dev Quang da ghe day", "Cool", "Mild", "Warmsssssssssss", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
